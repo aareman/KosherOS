71ee113011fa4f43637033a39f2b1965dc9b6757
Avi Areman <avi.areman@gmail.com>
2022-05-16 23:17:46 -0400

    Add etguardian config

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-NEW_USER=ruchniux
+_user=ruchniux
+_currentUser="$USER"
 
 
 # TODO: a module system to keep it clean and scalable
@@ -22,10 +23,49 @@ sudo apt upgrade -y
 sudo apt install docker.io docker-compose git -y
 
 # install nix
+sudo mkdir /nix
+sudo chown "$_currentUser"
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
 # Create a new sudo user to be able to revert this install
 
 # setup docker permissions
-#
 
-update sudoers file so custom apt can be run
+# NOT DOING: just removing sudo and sticking with nix
+# update sudoers file so custom apt can be run
+
+
+# TODO: build list
+# - grab from these lists
+# HOST FILES
+https://raw.githubusercontent.com/anudeepND/blacklist/master/facebook.txt
+https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser
+# NEED HOST PREFIX
+https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_top1m.list
+
+# TODO: Lock lists, waiting on working concept. This is the nsswitch thing
+
+function install_e2guardian() {
+    # clone project
+    git clone https://github.com/e2guardian/e2guardian.git
+    cd e2guardian
+    # install dependencies
+    sudo apt update && sudo apt install libtommath1 libevent-pthreads-2.1-6
+    sudo service e2guardian status
+    # configuration
+    # As sudo
+    mkdir -p /etc/e2guardian/ssl/generatedcerts
+    chown -R e2guardian. /etc/e2guardian/ssl
+    cd /etc/e2guardian/ssl
+    openssl genrsa 4096 > private_root.pem
+    openssl req -new -x509 -days 3650 -key private_root.pem -out my_rootCA.crt
+    # server needs to be the actual name of the server (here is 0.0.0.0)
+    openssl genrsa > private_cert.pem
+    chown -R e2guardian. /etc/e2guardian/ssl
+
+    # TODO: backup /etc/e2guardian
+
+}
+
+# Custom boot splash screen
+# https://askubuntu.com/a/2292
--- scripts/modules/packages.sh
+++ scripts/modules/packages.sh
@@ -6,3 +6,16 @@ sudo apt install docker.io docker-compose git -y
 # nix
 
 # setup docker permissions
+
+
+function install_e2guardian() {
+    git clone https://github.com/e2guardian/e2guardian.git
+    ./autogen.sh && ./configure  '--prefix=/usr' '--enabl
+e-clamd=yes' '--with-proxyuser=e2guardian' '--with-proxygrou
+p=e2guardian' '--sysconfdir=/etc' '--localstatedir=/var' '--
+enable-icap=yes' '--enable-commandline=yes' '--enable-email=
+yes' '--enable-ntlm=yes' '--mandir=${prefix}/share/man' '--i
+nfodir=${prefix}/share/info' '--enable-pcre=yes' '--enable-s
+slmitm=yes' 'CPPFLAGS=-mno-sse2 -g -O2' && make
+sudo make install
+}
